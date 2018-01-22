60f6b8ec6a337526a5ab28aa34b76faeb354cdef
Sahil <supadhyay@berkeley.edu>
2018-01-21 18:40:18 -0800

    sp18/lab/lab02.push

---
+++ materials/sp18/lab/lab02/tests/q442.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> all(annual_growth_rates == (changed/initials)-1)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
