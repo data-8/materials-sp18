a1ca71255176d7fee3596195d375446f82e759b2
Sahil <supadhyay@berkeley.edu>
2018-02-09 01:37:33 -0800

    sp18/project/project1.push

---
+++ materials/sp18/project/project1/tests/q1_12_0.py
@@ -0,0 +1,27 @@
+test = {
+  'name': 'Question 1_12_0',
+  'points': 0,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Check for correct output of helper function
+          >>> pop_for_year(1972) == 3345978384
+          True
+          >>> pop_for_year(1989) == 4567880153
+          True
+          >>> pop_for_year(2002) == 5501335945
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': False,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
