f2011143a819d2118bb00e959581a09cafa4577f
Sahil <supadhyay@berkeley.edu>
2018-02-19 12:49:47 -0800

    sp18/lab/lab05.push

---
+++ materials/sp18/lab/lab05/tests/q2_4.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> num_unique_words == 12943
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': False,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ materials/sp18/lab/lab05/tests/q2_5.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> num_q == 627
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': False,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
