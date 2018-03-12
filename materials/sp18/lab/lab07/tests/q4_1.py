ca1955843aa5e1879dc1b868ea122bf82fe1ea35
Sahil <supadhyay@berkeley.edu>
2018-03-11 21:22:38 -0700

    sp18/lab/lab07.push

---
+++ materials/sp18/lab/lab07/tests/q4_1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q4_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> chebyshev_upper_limit
+          0.25
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ materials/sp18/lab/lab07/tests/q4_2.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'q4_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> simulations.num_rows
+          50
+          >>> prob_winning < 0.1
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
