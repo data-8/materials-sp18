c5c93f59210db6cf9ce45212c6414d39e92da86b
Sahil <supadhyay@berkeley.edu>
2018-04-13 16:29:30 -0700

    sp18/hw/hw11.push

---
+++ materials/sp18/hw/hw11/tests/q3_12.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question 3_12',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Your choice should be one of the options
+          >>> question_12_choice in np.arange(1, 9)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ materials/sp18/hw/hw11/tests/q3_8.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question 3_8',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Make sure two_minutes_wait is a single value
+          >>> type(five_minutes_wait) == float
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
